660ab919e42fe765556a9159531e18c3c6706da9
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-13 23:05:50 +0100

    UPDATE efars package & app.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template("index.html")
+from efars import app
 
 if __name__ == '__main__':
     app.run()
