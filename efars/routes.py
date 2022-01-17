1a1351fc9b301586823b28a4519cd1c03d7e9360
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-17 22:42:44 +0100

    ADD about route

--- efars/routes.py
+++ efars/routes.py
@@ -4,3 +4,7 @@ from flask import Flask, render_template
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
