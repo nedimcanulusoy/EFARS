c1725bb513252fa245d027a2ea517c4533b2e0af
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-18 00:28:26 +0100

    ADD terms_conditions route

--- efars/routes.py
+++ efars/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, render_template
 def index():
     return render_template("index.html")
 
-@app.route('/about/')
+@app.route('/about/', methods=['GET'])
 def about():
     return render_template("about.html")
+
+@app.route('/terms-and-conditions/')
+def terms_conditions():
+    return render_template("terms.html")
