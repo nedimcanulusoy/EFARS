66adb1b37f36c66a6f41c26a90d77d7a2d5145cb
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-23 20:37:35 +0100

    ADD config settings

--- efars/__init__.py
+++ efars/__init__.py
@@ -2,4 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+if app.config['ENV'] == "production":
+    app.config.from_object("config.ProductConfig")
+# TODO elif statement can be added later
+else:
+    app.config.from_object("config.DevelopmentConfig")
+
 from efars import routes
