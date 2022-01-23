4ff21b7478745bddc3d0c4c4bfae173520abe704
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-23 22:49:08 +0100

    ADD file upload operation under index method

--- efars/routes.py
+++ efars/routes.py
@@ -1,9 +1,19 @@
 from efars import app
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from efars.forms import FileForm
+from werkzeug.utils import secure_filename
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template("index.html")
+    form = FileForm()
+
+    if form.validate_on_submit():
+        filename = secure_filename(form.file.data.filename)
+        print(filename)
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form)
+
 
 @app.route('/about/', methods=['GET'])
 def about():
@@ -16,3 +26,13 @@ def terms_conditions():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+# @app.route('/a', methods=['GET','POST'])
+# def upload_file():
+#     # if request.method == 'POST':
+#     #     uploaded_file = request.files['file']
+#     #     if uploaded_file.filename != '':
+#     #         print(f"{uploaded_file}")
+#     #         # uploaded_file.save(uploaded_file.filename)
+#     #     return redirect(url_for('index'))
+#     # return render_template("index.html")
