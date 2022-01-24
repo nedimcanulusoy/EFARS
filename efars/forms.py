8b0c6771976170b9b0d30a2c035e0b2c1002aeeb
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-24 17:09:18 +0100

    ADD file size limit validator

--- efars/forms.py
+++ efars/forms.py
@@ -1,12 +1,14 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileRequired, FileAllowed
+from flask_wtf.file import FileField, FileRequired, FileAllowed, FileSize
 from wtforms import SubmitField
 from config import Config
+
 
 class FileForm(FlaskForm):
     """
         FileAllowed(['allowed file extensions'])
         Verify file by extension based on what's inside FileAllow
     """
-    file = FileField('File',validators=[FileAllowed([*Config.ALLOWED_EXTENSIONS]),FileRequired()])
+    file = FileField('File', validators=[FileRequired(), FileAllowed([*Config.ALLOWED_EXTENSIONS]),
+                                         FileSize(max_size=Config.MAX_CONTENT_LENGTH)])
     submit = SubmitField()
