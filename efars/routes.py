8d6cf7b3e45f4e755f3cca538e0deeb69732d583
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-25 23:22:38 +0100

    UPDATE reformat file

--- efars/routes.py
+++ efars/routes.py
@@ -4,13 +4,14 @@ from flask import Flask, render_template, redirect, url_for, request, make_respo
 from werkzeug.utils import secure_filename
 from efars.utils import allowed_file, allowed_filesize, filesize
 
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+
 @app.route('/', methods=['POST'])
 def upload_file():
-
     uploaded_file = request.files['file']
     filename = secure_filename(uploaded_file.filename)
     if filename != '':
@@ -18,13 +19,13 @@ def upload_file():
 
         if allowed_file(filename) and allowed_filesize(filesize()):
             return {
-                'result':"OK"
-            },200
+                       'result': "OK"
+                   }, 200
         # return make_response(jsonify(result='OK'), 200) # Possible to return with make_response
         elif allowed_file(filename) and not allowed_filesize(filesize()):
-                return {
-                    'result':"SIZE ERROR"
-                },413
+            return {
+                       'result': "SIZE ERROR"
+                   }, 413
 
         else:
             return {
@@ -32,20 +33,24 @@ def upload_file():
                    }, 404
     else:
         return {
-            'result':"ERROR"
-        },404
+                   'result': "ERROR"
+               }, 404
+
 
 @app.route('/about/', methods=['GET'])
 def about():
     return render_template("about.html")
 
+
 @app.route('/terms-and-conditions/', methods=['GET'])
 def terms_conditions():
     return render_template("terms.html")
 
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
 
 @app.errorhandler(413)
 def file_size_exceed(e):
