7dd8b97ceed33cd628c143a61f02984431102aad
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-01-25 23:21:55 +0100

    UPDATE uploaded file control logic

--- efars/routes.py
+++ efars/routes.py
@@ -1,5 +1,8 @@
+import config
 from efars import app
 from flask import Flask, render_template, redirect, url_for, request, make_response
+from werkzeug.utils import secure_filename
+from efars.utils import allowed_file, allowed_filesize, filesize
 
 @app.route('/', methods=['GET'])
 def index():
@@ -9,12 +12,24 @@ def index():
 def upload_file():
 
     uploaded_file = request.files['file']
-    if uploaded_file.filename != '':
-        print(uploaded_file.filename)
-        return {
-            'result':"OK"
-        },200
+    filename = secure_filename(uploaded_file.filename)
+    if filename != '':
+        print(f"FILE: {uploaded_file.filename}")
+
+        if allowed_file(filename) and allowed_filesize(filesize()):
+            return {
+                'result':"OK"
+            },200
         # return make_response(jsonify(result='OK'), 200) # Possible to return with make_response
+        elif allowed_file(filename) and not allowed_filesize(filesize()):
+                return {
+                    'result':"SIZE ERROR"
+                },413
+
+        else:
+            return {
+                       'result': "FILE TYPE ERROR"
+                   }, 404
     else:
         return {
             'result':"ERROR"
