756ce571cf8b68872d174ddddac409c9efe940f6
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-03-07 22:48:27 +0100

    ADD folder_exists method

--- efars/utils.py
+++ efars/utils.py
@@ -1,8 +1,11 @@
-import config
+import os
+
 from flask import request
 
-def allowed_file(filename):
+import config
 
+
+def allowed_file(filename):
     if not "." in filename:
         return False
 
@@ -18,9 +21,17 @@ def filesize():
     size = len(size_request)
     return size
 
-def allowed_filesize(filesize):
 
+def allowed_filesize(filesize):
     if int(filesize) <= config.Config.MAX_CONTENT_LENGTH:
         return True
     else:
         return False
+
+
+def folder_exists():
+    default_path = config.Config.DEFAULT_PATH
+    is_exists = os.path.exists(default_path)
+
+    if not is_exists:
+        os.mkdir(default_path)
