7906106e8713d2a2a6da30709059a62876e67fad
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-03-26 22:50:52 +0100

    UPDATE remove unnecessary settings in config.py

--- config.py
+++ config.py
@@ -12,10 +12,6 @@ class Config(object):
 
     SECRET_KEY = config["secret_key"]
 
-    DB_NAME = config["db"]
-    DB_USERNAME = config["user"]
-    DB_PASSWORD = config["password"]
-
     ALLOWED_EXTENSIONS = ["csv"]
     MAX_CONTENT_LENGTH = 2 * 1000 * 1000  # File size limit is 16MB
     DEFAULT_PATH = config['default_folder_path']
@@ -25,25 +21,6 @@ class Config(object):
 class DevelopmentConfig(Config):
     DEBUG = True
 
-    DB_NAME = Config.config["development"]["db"]
-    DB_USERNAME = Config.config["development"]["user"]
-    DB_PASSWORD = Config.config["development"]["password"]
-
 
 class ProductConfig(Config):
     DEBUG = False
-
-    DB_NAME = Config.config["production"]["db"]
-    DB_USERNAME = Config.config["production"]["user"]
-    DB_PASSWORD = Config.config["production"]["password"]
-
-
-class TestingConfig(Config):
-    pass
-
-
-# TODO Upgrade to Instance folder type in the future
-"""
-Documentation: http://exploreflask.com/en/latest/configuration.html
-Documentation: https://pythonise.com/series/learning-flask/flask-configuration-files
-"""
