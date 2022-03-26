59c8c6de8676badc3e283416d4868cc80af7d268
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-03-26 22:11:00 +0100

    ADD showError and fadeOut function to core.js

--- efars/static/js/core.js
+++ efars/static/js/core.js
@@ -48,3 +48,46 @@ function downloadPDF() {
     // showUploadView()
 }
 
+function showError(message) {
+    const alertPlaceholder = document.getElementById('errorAlertPlaceholder')
+    const wrapper = document.createElement('div')
+
+    wrapper.innerHTML = `<div class="alert alert-danger alert-dismissible" role="alert">
+                            ${message}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`
+
+    alertPlaceholder.append(wrapper)
+
+    setTimeout(() => {
+        fadeOut(wrapper, 500)
+    }, 3000)
+}
+
+function fadeOut(elem, ms) {
+    if (!elem)
+        return;
+
+    if (ms) {
+        var opacity = 1;
+        var timer = setInterval(function () {
+            opacity -= 50 / ms;
+            if (opacity <= 0) {
+                clearInterval(timer);
+                opacity = 0;
+                elem.style.display = "none";
+                elem.style.visibility = "hidden";
+            }
+            elem.style.opacity = opacity;
+            elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+        }, 50);
+    } else {
+        elem.style.opacity = 0;
+        elem.style.filter = "alpha(opacity=0)";
+        elem.style.display = "none";
+        elem.style.visibility = "hidden";
+    }
+}
+
+// https://stackoverflow.com/questions/13733912/javascript-fade-in-fade-out-without-jquery-and-css3
+//https://stackoverflow.com/questions/3357553/how-do-i-store-an-array-in-localstorage
