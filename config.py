ab8b3c073c4eda85ee4d0c1380f38eca6c522f2e
TheNavyInfantry <nedimcanulusoy@gmail.com>
2022-03-29 22:41:25 +0200

    ADD auto generated config file feature

--- config.py
+++ config.py
@@ -1,16 +1,32 @@
 import os
+import secrets
 
 import yaml
 
 
 class Config(object):
-    stream = open(os.getcwd() + "/config.yml", 'r')
+    config_path = os.getcwd() + "/config.yml"
+    file_exists = os.path.exists(config_path)
+
+    if not file_exists:
+        data = dict(
+            secret=secrets.token_hex(32),
+
+            default_folder_path="efars/process/",
+            plot_folder_path="plots/",
+            result_folder_path="result/",
+        )
+
+        with open(config_path, 'w') as outfile:
+            yaml.dump(data, outfile, default_flow_style=False)
+
+    stream = open(config_path, 'r')
     config = yaml.load(stream, Loader=yaml.FullLoader)
 
     DEBUG = False
     TESTING = False
 
-    SECRET_KEY = config["secret_key"]
+    SECRET_KEY = config['secret']
 
     ALLOWED_EXTENSIONS = ["csv"]
     MAX_CONTENT_LENGTH = 2 * 1000 * 1000
